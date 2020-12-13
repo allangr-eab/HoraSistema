8fa88624600069d3445a51b2886c23fe8eb072c9
Allangr-eab <allangabrielrvsantos@yahoo.com.br>
2020-12-13 03:57:07 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("---Data e hora atual do sistema");
+            DateTime HoraSistema = DateTime.Now;
+            Console.WriteLine( "Data e hora atual do sistema {HoraSistema}");
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HoraSistema
 Exibição da data e a hora atual do sistema
+
+**Enunciado**:
+
+Faça um programa que exiba a data e a hora do sistema: `13/12/2020 03:27´
